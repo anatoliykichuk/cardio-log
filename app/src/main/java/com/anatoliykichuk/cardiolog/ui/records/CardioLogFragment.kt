9dc5ecc8709bfdc1d6147fd43dc5403b053c97bc
anatoliy <anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.comgit config --global user.email anatoliy.kichuk@gmail.com>
2024-02-22 14:51:02 +0200

    Выполнен рефакторинг изменения данных

--- app/src/main/java/com/anatoliykichuk/cardiolog/ui/records/CardioLogFragment.kt
+++ app/src/main/java/com/anatoliykichuk/cardiolog/ui/records/CardioLogFragment.kt
@@ -12,10 +12,11 @@ import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.RecyclerView
 import com.anatoliykichuk.cardiolog.databinding.FragmentCardioLogBinding
 import com.anatoliykichuk.cardiolog.domain.CardioLog
-import com.anatoliykichuk.cardiolog.ui.state.AppState
 import com.anatoliykichuk.cardiolog.ui.adapter.CardioLogAdapter
 import com.anatoliykichuk.cardiolog.ui.adapter.ICardioLogOnRecordDataChangeListener
+import com.anatoliykichuk.cardiolog.ui.state.AppState
 import com.anatoliykichuk.cardiolog.ui.state.CardioLogEventType
+import com.anatoliykichuk.cardiolog.ui.state.ResponseData
 
 class CardioLogFragment : Fragment(), ICardioLogOnRecordDataChangeListener {
 
@@ -66,25 +67,7 @@ class CardioLogFragment : Fragment(), ICardioLogOnRecordDataChangeListener {
         viewModel.getLiveData().observe(viewLifecycleOwner) {
             when (it) {
                 is AppState.Success -> {
-                    val responseData = it.responseData
-
-                    if (responseData.records != null) {
-                        records = responseData.records
-                    } else if (responseData.record != null) {
-                        if (responseData.eventType == CardioLogEventType.REMOVING) {
-                            if (records.indexOf(responseData.record) > 0) {
-                                records.remove(responseData.record)
-                            }
-                        }
-                    } else {
-                        return@observe
-                    }
-
-                    records.sortBy { it.date }
-                    adapter = CardioLogAdapter(records, this)
-
-                    cardioLogRecyclerView.setHasFixedSize(true)
-                    cardioLogRecyclerView.adapter = adapter
+                    processResponseData(it.responseData)
                 }
 
                 is AppState.Error -> {
@@ -132,7 +115,6 @@ class CardioLogFragment : Fragment(), ICardioLogOnRecordDataChangeListener {
         binding.addRecordFab.setOnClickListener { view ->
             val record = CardioLog()
             viewModel.addRecord(record)
-            //records.add(record)
         }
 
         binding.removeRecordFab.setOnClickListener { view ->
@@ -142,12 +124,49 @@ class CardioLogFragment : Fragment(), ICardioLogOnRecordDataChangeListener {
 
             val record = records[currentPosition]
             viewModel.removeRecord(record)
-            //records.remove(record)
         }
     }
 
     override fun onChanged(cardioLog: CardioLog) {
         viewModel.updateRecord(cardioLog)
+    }
+
+    private fun processResponseData(responseData: ResponseData) {
+        if (responseData.records != null) {
+            records = responseData.records
+        } else if (!recordsUpdatedByEventType(responseData)) {
+            return
+        }
+
+        records.sortBy { it.date }
+        adapter = CardioLogAdapter(records, this)
+
+        cardioLogRecyclerView.setHasFixedSize(true)
+        cardioLogRecyclerView.adapter = adapter
+    }
+
+    private fun recordsUpdatedByEventType(responseData: ResponseData): Boolean {
+        if (responseData.record == null) {
+            return false
+        }
+
+        if (responseData.eventType == CardioLogEventType.ADDING) {
+            records.add(responseData.record)
+            return true
+        }
+
+        val indexOfRecord = records.indexOf(responseData.record)
+
+        if (indexOfRecord < 0) {
+            return false
+        }
+
+        if (responseData.eventType == CardioLogEventType.UPDATING) {
+            records[indexOfRecord] = responseData.record
+        } else if (responseData.eventType == CardioLogEventType.REMOVING) {
+            records.remove(responseData.record)
+        }
+        return true
     }
 
     companion object {
